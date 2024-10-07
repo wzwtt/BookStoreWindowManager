aff1501935f5a89b88bd7bb244b620fbffc34548
Congyue Wang (PGT) <c4036215@newcastle.ac.uk>
2024-10-07 13:55:23 +0100

    20241007 pratical

--- src/csc8011/BookStoreWindow.java
+++ src/csc8011/BookStoreWindow.java
@@ -1,5 +1,50 @@
 package csc8011;
 
 public class BookStoreWindow {
-    
+    private String name;
+    private Book[] books = new Book[10000];
+    private final int number;
+
+    public BookStoreWindow(String name, Book[] books, int number) {
+        this.name = name;
+        this.books = books;
+        this.number = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Book[] maxValue() {
+        Book max = new Book("0", "", 0, 0.0);
+        for (int i = 0; i < number; i++) {
+            if (books[i].getValue() > max.getValue()) {
+                max = books[i];
+            }
+        }
+        return new Book[]{max};
+    }
+
+    public Book[] oldestBook() {
+        Book oldest = new Book("0", "", 0, 0.0);
+        for (int i = 0; i < number; i++) {
+            if (books[i].getYear() < oldest.getYear()) {
+                oldest = books[i];
+            }
+        }
+        return new Book[]{oldest};
+    }
+
+
+    public double averageValue() {
+        double average = 0;
+        for (int i = 0; i < number; i++) {
+            average += books[i].getValue();
+        }
+        return average / number;
+    }
 }
--- src/csc8011/BookStoreWindowIO.java
+++ src/csc8011/BookStoreWindowIO.java
@@ -1,5 +1,52 @@
 package csc8011;
 
+import java.util.*;
+import java.io.*;
+
 public class BookStoreWindowIO {
 
+    static String ShopName = "";
+    static Scanner in = new Scanner(System.in);
+    static Book[] books = new Book[10000];
+
+    public static String showOptions() {
+        String choice = "";
+        while (!(choice.equals("1") || choice.equals("2") || choice.equals("3") || choice.equals("4"))) {
+            System.out.println("Book Store Window");
+            System.out.println("1. Enter the name of the book store");
+            System.out.println("2. Read in information on the books from a .csv file in the current directory called books.csv.");
+            System.out.println("3. Print a summary of the book store name followed by a list of all books, their value and the year published to the console.");
+            System.out.println("4. Print statistics on books, showing the full details of the book with the highest value, oldest book displayed in the window, and average value of books displayed in the window of the book store, to the console");
+            System.out.println("5. Exit");
+            System.out.println();
+            System.out.println("Enter your choice: ");
+            choice = in.nextLine();
+            if (!(choice.equals("1") || choice.equals("2") || choice.equals("3") || choice.equals("4"))) {
+                System.out.println("Invalid choice. Try again.");
+                System.out.println();
+            }
+        }
+        return choice;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int number =0;
+        switch (showOptions()) {
+            case "1":
+                System.out.println("Enter the name of the book store: ");
+                ShopName = in.nextLine();
+                System.out.println("Accepted");
+                break;
+            case "2":
+                File file = new File("books.csv");
+                Scanner ReadFile = new Scanner(file);
+                while (ReadFile.hasNextLine()) {
+                    String data = ReadFile.nextLine();
+                    books[number].setID()=;
+                    number++;
+                }
+                ReadFile.close();
+        }
+
+    }
 }
